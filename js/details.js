34441df9f6a85a47ef2c17154e5379ae047c1cc8
Christinalu <89390143+Christinalulu@users.noreply.github.com>
2022-04-24 13:40:50 +0200

    did some fixing on the code

--- js/details.js
+++ js/details.js
@@ -1,23 +1,18 @@
 const querystring = document.location.search;
 const params = new URLSearchParams(querystring);
 const exhibitionName = params.get('name');
-const new_URL = `https://spooky-flock.flywheelsites.com/wp-json/wc/store/products/${name}`;
-console.log(new_URL);
-
+const new_URL = `https://spooky-flock.flywheelsites.com//wp-json/wc/store/products${name}`;
 const exhibitionDetails = document.querySelector('.exhibitions-details');
-
 async function getDetail() {
   try {
     const responseDetail = await fetch(new_URL);
     const responseDetailJSON = await responseDetail.json();
 
     for (let i = 0; i < responseDetailJSON.length; i++) {
-      console.log(responseDetailJSON[i]);
       const detailName = responseDetailJSON[i].name;
       const detailInfo = responseDetailJSON[i].description;
       const img = responseDetailJSON[i].images[0].src;
       const altText = responseDetailJSON[i].images[0].alt;
-
       exhibitionDetails.innerHTML = `
       <div class="exhibiton-container"
       <div class="description"><span>${detailName}</span>${detailInfo}</div>
--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 const MyUrl =
-  'https://spooky-flock.flywheelsites.com/wp-json/wc/store/products';
+  'https://spooky-flock.flywheelsites.com//wp-json/wc/store/products';
 const exhibitonContainer = document.querySelector('.exhibitions');
-
 async function fetchExhibitions() {
   try {
     const response = await fetch(MyUrl);
     const responseJson = await response.json();
     const myDetails = responseJson;
-    console.log(response);
     for (let i = 0; i < myDetails.length; i++) {
       let exhibitionImg = myDetails[i].images[0].src;
       let altText = myDetails[i].images[0].alt;
