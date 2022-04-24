8e5f1cb09c64d8d3888c14d7ec197a3d0fc3a7d3
Christinalu <89390143+Christinalulu@users.noreply.github.com>
2022-04-24 11:33:29 +0200

    linket sammen sider

--- js/script.js
+++ js/script.js
@@ -6,20 +6,19 @@ async function fetchExhibitions() {
   try {
     const response = await fetch(MyUrl);
     const responseJson = await response.json();
-    console.log('My respnse JSON', responseJson);
     const myDetails = responseJson;
-
+    console.log(response);
     for (let i = 0; i < myDetails.length; i++) {
       let exhibitionImg = myDetails[i].images[0].src;
       let altText = myDetails[i].images[0].alt;
       let descriptionInfo = myDetails[i].short_description;
       let readMore = myDetails[i].add_to_cart.text;
-      let exhibtionsName = myDetails[i].name;
+      let exhibitionsName = myDetails[i].name;
       exhibitonContainer.innerHTML += `
       <div class="exhibiton-container"
-      <div class="description"><span>${exhibtionsName}</span>${descriptionInfo}</div>
+      <div class="description"><span>${exhibitionsName}</span>${descriptionInfo}</div>
       <img class="exhibtion-img" src="${exhibitionImg}" alt="${altText}">
-     <div class="button-read-more"><a href=exhibitions-details.html?name=${exhibtionsName}"</a>${readMore}</div>
+     <div class="button-read-more"><a href=exhibitions-details.html?name=${exhibitionsName}"</a>${readMore}</div>
       </div>
       `;
     }
